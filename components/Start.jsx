b64ec053678763fa1413e97584b4a477b510cf38
Adarsh Kumar <adarsh010c@gmail.com>
2023-02-18 02:04:13 +0530

    Completed the start page

--- components/Start.jsx
+++ components/Start.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import  BlobA from '../src/assets/Polygon_1.svg'
+import BlobB from '../src/assets/Polygon_2.svg'
 
 export default function Start(){
     return(
         <div className="start">
-            <div className="start__blob">
-                
+            <div className="start__blobA">
+                <img src={BlobA} class="start__blobA-img" alt="Blob" />
             </div>
             <h1 className="start__header">
                 Quizzical
@@ -15,6 +17,9 @@ export default function Start(){
             <a href="#" className="start__btn">
                 Start quiz
             </a>
+            <div className="start__blobB">
+                <img src={BlobB} class="start__blobB-img" alt="Blob2" />
+            </div>
         </div>
     )
 }
